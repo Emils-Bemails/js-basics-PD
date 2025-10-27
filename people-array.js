8f5d1521dd796c0cf1855846d0a299f647a552fe
unknown <deivis.stasuls@gmail.com>
2025-10-27 11:24:44 +0200

    People Array uzdevums izpildits

---
+++ people-array.js
@@ -0,0 +1,16 @@
+let people = [
+    {name: "Jānis",
+        age: 15,
+        isStudent: true
+    },
+    {name: "Emīls",
+        age: 25,
+        isStudent: false
+    },
+];
+
+
+
+for (let i = 0; i < people.length; i++) {
+    console.log("Vārds: " + people[i].name);
+}
