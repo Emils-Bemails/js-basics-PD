5aa4004a933dbef63506806128882f6aaed93185
unknown <deivis.stasuls@gmail.com>
2025-10-27 10:52:07 +0200

    Ticket uzdevums izpildits

---
+++ ticket-price.js
@@ -0,0 +1,10 @@
+let visitorAge = 5;
+let isStudent = true;
+
+if (visitorAge <= 6) {
+    console.log("Biļetes centa:  Bez maksas")
+} else if (isStudent === true) {
+    console.log("Biletes cena: 5 Euro")
+} else {
+    console.log("Biletes maksa: 10 euro")
+}
